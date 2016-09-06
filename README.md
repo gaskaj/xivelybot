253dec175d87066c070d37c23f77782a47772ac5
gaskaj <joegaska@gmail.com>
2016-09-06 11:05:36 -0400

    new fix

---
+++ README.md
@@ -0,0 +1,10 @@
+# Xivelybot
+
+This is a demo how to extend Xively and Salesforce using Botkit framework tightly coupling the business integration
+framework with IoT.
+
+### Bot Testing UI
+http://xivelybotui.herokuapp.com/
+
+### Bot Deployed
+http://xivelybot.herokuapp.com/
--- config/index.js
+++ config/index.js
@@ -4,12 +4,6 @@ const config = {
     logger: {
         level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'development' ? 'debug' : 'info')
     },
-    salesforce: {
-        user: process.env.SALESFORCE_USER,
-        password: process.env.SALESFORCE_PASSWORD,
-        token: process.env.SALESFORCE_TOKEN,
-        url: process.env.SALESFORCE_URL
-    },
     bot:{
         twilio: {
             account_sid: process.env.TWILIO_ACCOUNT_SID,
